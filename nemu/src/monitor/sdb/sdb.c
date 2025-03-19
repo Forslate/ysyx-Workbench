6c5391b56044b01f30fe0e573dae5123ccb9ac6d
Feng Sidi <161752661+Forslate@users.noreply.github.com>
2025-03-19 22:29:23 +0800

    Docs@docs(sdb.c): 对 rl_gets() 函数和 部分 `cmd_table[]` 的注释
    Reason：
        - 加强对 sdb.c 的掌握

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -13,24 +13,30 @@
 * See the Mulan PSL v2 for more details.                                                          *
 **************************************************************************************************/
 
-/***************************************** include files *****************************************/
+
+/*======================================== Header Files =========================================*/
+/// Project headers
 #include <isa.h>
 #include <cpu/cpu.h>
 
+/// System Libraries with GNU C Library
 #include <readline/readline.h>
 #include <readline/history.h>
 
+/// Project specific modules
 #include "sdb.h"
 #include "utils.h"
-/***************************************** include files *****************************************/
+/*======================================== Header Files =========================================*/
 
+/*======================================= Global Settings =======================================*/
 /// 是否为批处理模式，这里为 `否`
 static int is_batch_mode = false;
+/*======================================= Global Settings =======================================*/
 
 void init_regex();
 void init_wp_pool();
 
-/******************************** function prototype declaration *********************************/
+/*===================================== Function Prototypes =====================================*/
 static char *rl_gets();
 
 static int cmd_c(char *args);
@@ -40,9 +46,9 @@ static int cmd_help(char *args);
 void sdb_set_batch_mode();
 void sdb_mainloop();
 void init_sdb();
-/******************************** function prototype declaration *********************************/
+/*===================================== Function Prototypes =====================================*/
 
-/********************************** rl_gets function definition **********************************/
+/*================================= rl_gets function definition =================================*/
 /* We use the `readline` library to provide more flexibility to read from stdin. */
 
 /// rl_gets == readline gets ?
@@ -68,7 +74,18 @@ static char *rl_gets()
 
     /// 双重检查：
     /// line_read 不为空指针（确保从 `readline` 获取输入）。
+    /// `line_read` 是指向字符的指针，存储了从 `readline` 获取的用户输入字符串的地址。
+    /// C语言中，指针变量可以指向内存中的某个地址，或者为 `NULL`。
+    /// 所以可能性为：非空指针（指向用户输入字符串的首地址）或者为 `NULL`。
+    /// 首先检查 `line_read` 是否为非空，如果是，则继续检查第二个条件。
+    ///
     /// *line_read 不是空字符（用户没有输入空行）。
+    /// 如果 `line_read` 非空，则意味着指向了一个有效的字符串地址，`*line_read` 是对该指针的解引用，
+    /// 即访问指针指向的内存位置的值，对于字符串是检查字符串的第一个字符。
+    /// 字符串在C语言中以空字符（\0）结尾。
+    /// 如果用户直接键入回车键（没有输入任何内容），`readline` 会返回一个空字符串（即一个半酣单个
+    /// `\0` 的字符串）。
+    /// 在ASCII中对应的是0，在条件判断中视为 假。
     if (line_read && *line_read) {
         /// 如果通过检查，则将输入添加到历史记录中。
         add_history(line_read);
@@ -78,12 +95,22 @@ static char *rl_gets()
     /// 不需要释放该内存，会在下一次调用本函数时自动释放。
     return line_read;
 }
-/********************************** rl_gets function definition **********************************/
+/*================================= rl_gets function definition =================================*/
 
-/*************************************************************************************************/
+/*=========================================== cmd_table =========================================*/
+/// 匿名结构体（没有类型名），直接用于数组定义
+/// 适合一次性使用的结构
 static struct {
+    /// 不可修改的字符串指针
+    /// 存储命令的触发名称
+    /// 例如输入 "help" 时会匹配该字段，查找 `name` 为 "help" 的条目
     const char *name;
+    /// 帮助信息显示
     const char *description;
+    /// 函数指针：接受 char * 的参数，返回 `int` 的参数
+    /// `int`：返回值类型
+    /// `(*handler)`：指针变量名
+    /// `(char *)`：参数类型（指向命令参数的字符串。
     int (*handler) (char *);
 } cmd_table [] = {
     {.name = "help", .description = "Display information about all supported commands", .handler = cmd_help},
@@ -94,7 +121,7 @@ static struct {
 };
 
 #define NR_CMD ARRLEN(cmd_table)
-/*************************************************************************************************/
+/*=========================================== cmd_table =========================================*/
 
 /*************************************************************************************************/
 static int cmd_c(char *args)
